e2005f0ca03668480a1a46570dd4cf7bc3a921b8
용석 박 <4up456@naver.com>
2022-01-18 23:08:04 +0900

    mbti별 css 색상 및 효과 지정

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log('hi');
+const mbtiSection = document.querySelector('.mbti__section');
+
+mbtiSection.addEventListener('mouseover', (event) => {
+  const alphabet = event.target.dataset.in;
+  const mbtiDescription = document.querySelector(
+    `.mbti__description[data-out="${alphabet}"]`
+  );
+  if (mbtiDescription === null) return;
+  mbtiDescription.classList.remove('hidden');
+});
+
+mbtiSection.addEventListener('mouseout', (event) => {
+  const alphabet = event.target.dataset.in;
+  const mbtiDescription = document.querySelector(
+    `.mbti__description[data-out="${alphabet}"]`
+  );
+  if (mbtiDescription === null) return;
+  mbtiDescription.classList.add('hidden');
+});
